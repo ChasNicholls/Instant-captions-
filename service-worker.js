80a6923e77550e9654293228af09cb7cf3c4fbe7
ChasNicholls <46545822+ChasNicholls@users.noreply.github.com>
2025-09-08 15:00:06 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,6 @@
 // Simple offline cache for PWA shell
-const CACHE = 'instant-captioner-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  e.respondWith(caches.match(req).then(res => res || fetch(req)));
-});
+const CACHE = 'instant-captioner-ll-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
